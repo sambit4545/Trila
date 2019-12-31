37b4c2db17ea821e25253617aea22991fdce7e33
sambit4545 <51986229+sambit4545@users.noreply.github.com>
2019-12-31 23:43:38 +0530

    Project started
    New project

---
+++ GitDemoConsoleApp/GitDemoConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1st line");
+            Console.ReadLine();
+        }
+    }
+}
